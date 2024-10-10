c817ffe43be739fcd681740e157ac0f403784b6a
TaraLavanya <lavanyatara15@gmail.com>
2024-10-10 14:43:13 +0530

    feat: files created

---
+++ app/Http/Livewire/CategoryHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CategoryHandler extends Component
+{
+    public function render()
+    {
+        return view('livewire.category-handler');
+    }
+}
